af27320c7b10010f919f1c7a336531fe120a17e5
GerGerson <jasonlai11281983@gmail.com>
2014-03-12 23:02:16 +0800

    v0.0.14
    Fix Browser CSS problem

---
+++ routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+/**********Login********/
+Route::post('/login/login_check', 'RouteController@LoginCheck');
+
+Route::group(array('before' => 'login_auth_check'), function()
+{
+	Route::get('/', '');
+	Route::get('/login', '');
+});
+
+Route::group(array('before' => 'quotation_auth_check'), function()
+{
+	Route::get('/quotation', 'QuotationController@index');
+	Route::get('/quotation/{id}', 'QuotationController@detail');
+});
+
+Route::get('/logout', 'RouteController@Logout');
+/**********Login********/
