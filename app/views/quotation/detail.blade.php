a4c18e86d753883f19c5cfe0e3e580d30ac48d57
Lau Yuet Chung <lauyuetchung@gmail.com>
2014-03-13 00:38:36 +0800

    v0.0.15 Quotation Detail layout updated

--- app/views/quotation/detail.blade.php
+++ app/views/quotation/detail.blade.php
@@ -26,20 +26,72 @@
 		</div>
 	</div>
 	<hr/>
-	<div class="col-xs-offset-2" style="margin-top:-20px">
+	<div class="col-xs-offset-2" style="margin-top:-20px ">
 		<div class="row">
-			<div class="col-xs-5">
-			
-				<h4>報價單詳情</h4>
-				<ul class="list-unstyled">
-					<li>
-						{{$quotation->quotation_name}}
-					</li>
-				</ul>
+			<div class="col-xs-5" style="font-size:15px">
+				<table align="center">
+					<tr>
+						<td>報價單名稱 : &nbsp;&nbsp;</td>
+						<td>{{$quotation->quotation_name}}</td>
+					</tr>
+					<tr>
+						<td>報價單狀態 : &nbsp;&nbsp;</td>
+						@if ($quotation->status == 0)
+							<td>審核中</td>
+						@else
+							<td>已審核</td>
+						@endif
+					</tr>
+				</table>
+
 			</div>
-			<div class="col-xs-5 invoice-payment">
-				<h4>備註</h4>
-				<ul class="list-unstyled" style="font-size: 12px; text-align:left">
+			
+			<div class="col-xs-5" style="font-size:15px">
+				<table align="center">
+					<tr>
+						<td>客戶 : &nbsp;&nbsp;</td>
+						<td>{{$quotation->give_to}}</td>
+					</tr>
+					<tr>
+						<td>跟進師傅 : &nbsp;&nbsp; </td>
+						<td>{{$quotation->contact_to}}</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-10">
+				<table class="table table-striped table-hover" >
+					<thead>
+						<tr>
+							<th style="text-align:right;">#</th>
+							<th style="text-align:left;">
+								項目名稱
+							</th>
+							<th class="hidden-480" style="text-align:right;">
+								價錢
+							</th>
+						</tr>
+					</thead>
+					<tbody >
+						<?php $total = 0; 
+								$count = 0;?>
+						@foreach ($items as $item)
+							<?php $total += $item->item_price; 
+									$count++;?>
+							<tr>
+								<td style="text-align:right; padding-top: 4px; padding-bottom: 4px;" >{{$count}}</td>
+								<td style="text-align:left; padding-top: 4px; padding-bottom: 4px;">{{$item->item_name}}</td>
+								<td style="text-align:right; padding-top: 4px; padding-bottom: 4px;"><?php echo number_format($item->item_price, 2);?></td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-10 invoice-payment">
+				<ul class="list-unstyled" style="font-size: 14px; text-align:left">
 					@if ($quotation->remark_1 != "")
 						<li>
 							備註1 : {{$quotation->remark_1}}
@@ -69,36 +121,6 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-xs-10">
-				<table class="table table-striped table-hover">
-					<thead>
-						<tr>
-							<th style="text-align:right;">#</th>
-							<th style="text-align:left;">
-								項目名稱
-							</th>
-							<th class="hidden-480" style="text-align:right;">
-								價錢
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php $total = 0; 
-								$count = 0;?>
-						@foreach ($items as $item)
-							<?php $total += $item->item_price; 
-									$count++;?>
-							<tr>
-								<td style="text-align:right;">{{$count}}</td>
-								<td style="text-align:left;">{{$item->item_name}}</td>
-								<td style="text-align:right;">$<?php echo number_format($item->item_price, 2);?></td>
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
-			</div>
-		</div>
-		<div class="row">
 			<div class="col-xs-5">
 				<div class="well">
 					<p>
@@ -124,8 +146,40 @@
 					</tbody>
 				</table>
 				<br/>
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-5">
+				<p>
+					公司簽署
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					_______________________________________
+				</p>
+				
+			</div>
+			<div class="col-xs-5">
+				<p>
+					客戶簽署
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					_______________________________________
+				</p>
+			</div>
+			<br/>
+		</div>
+		<div class="row">
+			<div class="col-xs-5">
+			</div>
+			<div class="col-xs-5" style="text-align: right;">
 				<a class="btn btn-lg blue hidden-print" onclick="javascript:window.print();">列印 <i class="fa fa-print"></i></a>
 			</div>
+			</br>
 		</div>
 	</div>
 	<!-- END PAGE CONTENT-->
